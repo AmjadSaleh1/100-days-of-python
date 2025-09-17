393938a389fad484fc4ead888cc2dcfb14e5eafc
Amjad Salih <amjadsalih1809@gmail.com>
2025-09-18 00:46:48 +0300

    updating the code to Book classes

--- day44/main.py
+++ day44/main.py
@@ -2,8 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.common.exceptions import NoSuchElementException
 from time import sleep
+
+booked_counter = 0
+listed = 0
+
 
 URL = "https://appbrewery.github.io/gym/"
 
@@ -13,6 +16,7 @@ chrome_option.add_experimental_option("detach", True)
 driver = webdriver.Edge(options=chrome_option)
 wait = WebDriverWait(driver, 10)
 
+#entering the website and logging in.
 driver.get(URL)
 login_button = driver.find_element(By.CLASS_NAME, value="Navigation_button__uyKX2")
 login_button.click()
@@ -23,15 +27,30 @@ password_input.send_keys("password123")
 submit_button = driver.find_element(By.CLASS_NAME, value="Login_submitButton__tJFna ")
 submit_button.click()
 
+#start booking
 sleep(5)
 day_title = driver.find_element(By.XPATH, '//*[@id="day-title-tue,-sep-23"]')
 day_div = day_title.find_element(By.XPATH, './..')
 
-for card in day_div.find_elements(By.CLASS_NAME, "ClassCard_card__qx5"):
+for card in day_div.find_elements(By.CLASS_NAME, "ClassCard_card__KpCx5"):
     class_time = card.find_element(By.CLASS_NAME, "ClassCard_classDetail__Z8Z8f").text
 
     if "9:00 AM" in class_time:
         button = card.find_element(By.TAG_NAME, "button")
-        button.click()
-        print("Clicked!")
-        break
+        if button.text == "Booked":
+            booked_counter += 1
+            print("already booked")
+            break
+
+        if button.text == "Waitlisted":
+            print("u are on the waitlist")
+
+        if button.text == "Join Waitlist":
+            button.click()
+            print("you joined to the waitlist")
+            break
+
+        else:
+            button.click()
+            print("Clicked!")
+            break
